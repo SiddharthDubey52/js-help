6c5f5b9ad22fd544fbf517d90056811ed3d7c0be
Siddharthdubey52 <dubeysiddharth368@gmail.com>
2024-11-13 23:39:51 +0530

    Day-2

---
+++ function/function-2/app.js
@@ -0,0 +1,12 @@
+//This Keyword
+const student = {
+    name : "rahul",
+    age : 19,
+    class : "9th",
+    math : 53,
+    sci : 98,
+    testavg(){
+        let avg = (this.math,this.sci)/2;
+        console.log(avg);
+    }
+}// 49
