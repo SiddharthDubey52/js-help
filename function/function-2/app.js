d3676e3a0532412d051a89fc8cda500281758224
Siddharthdubey52 <dubeysiddharth368@gmail.com>
2024-11-14 00:05:41 +0530

    day-02-1

--- function/function-2/app.js
+++ function/function-2/app.js
@@ -1,12 +1,28 @@
 //This Keyword
-const student = {
-    name : "rahul",
-    age : 19,
-    class : "9th",
-    math : 53,
-    sci : 98,
-    testavg(){
-        let avg = (this.math,this.sci)/2;
-        console.log(avg);
-    }
-}// 49
+// const student = {
+//     name : "rahul",
+//     age : 19,
+//     class : "9th",
+//     math : 53,
+//     sci : 98,
+//     testavg(){
+//         let avg = (this.math,this.sci)/2;
+//         console.log(avg);
+//     }
+// }// 49
+
+// try and catch 
+console.log("hello");
+console.log("hello");
+console.log("hello");
+try{
+    console.log(a);
+}
+catch{
+    console.log("it caught error.....")
+}
+
+console.log("hello");
+console.log("hello");
+console.log("hello");
+
