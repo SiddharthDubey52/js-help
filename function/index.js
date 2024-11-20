0d01ce4c3948bc174f7bfe0f95b6bc1d5ab760f9
Siddharthdubey52 <dubeysiddharth368@gmail.com>
2024-11-20 17:42:52 +0530

    teach4

--- function/index.js
+++ function/index.js
@@ -37,44 +37,98 @@
 // let [,[a,,[b,[,,[c]]]]] = arr6;
 // console.log(a,b,c);
 
-let obj = {
-    id:1,
-    tittle:"Laptop",
-    brand:"HP",
-    price:70000,
-};
+// let obj = {
+//     id:1,
+//     tittle:"Laptop",
+//     brand:"HP",
+//     price:70000,
+// };
 
-let{tittle} = obj;
-console.log(tittle);
+// let{tittle} = obj;
+// console.log(tittle);
 
-let obj1 = {
-    id:1,
-    title : "laptop",
-    price: 109.95,
-    category:"Electronics",
-    rating:{
-        rate:3.9,
-        count:120,
-    },
-};
-let{title,category,rating:{count,rate}} = obj1;
-console.log(title,category,count);
+// let obj1 = {
+//     id:1,
+//     title : "laptop",
+//     price: 109.95,
+//     category:"Electronics",
+//     rating:{
+//         rate:3.9,
+//         count:120,
+//     },
+// };
+// let{title,category,rating:{count,rate}} = obj1;
+// console.log(title,category,count);
 
 
-let obj2 ={
- id:2,
- ename:"raj",
- hobbies: ["cricket","football"],
- skills:{
-    frontend:["html",'css','js','react'],
-    backend:['node','java','sql',"mongo",'express'],
- },
-};
+// let obj2 ={
+//  id:2,
+//  ename:"raj",
+//  hobbies: ["cricket","football"],
+//  skills:{
+//     frontend:["html",'css','js','react'],
+//     backend:['node','java','sql',"mongo",'express'],
+//  },
+// };
 
-let{ename,hobbies:[,a],skills:{frontend:[,,,b],backend:[,c,,,]}} = obj2;
-console.log(a,b,c);
+// let{ename,hobbies:[,a],skills:{frontend:[,,,b],backend:[,c,,,]}} = obj2;
+// console.log(a,b,c);
 
-function test({id,ename},x){
-  console.log(id,ename,x);
-}
-test({id:1,ename:"raj"},100);
+// function test({id,ename},x){
+//   console.log(id,ename,x);
+// }
+// test({id:1,ename:"raj"},100);
+
+
+
+// let  x = fetch("https://api.github.com/users")
+// console.log(x)
+
+// Promise
+// let data = [{id:1,ename:"john"},{id:2,ename:"wick"}];
+
+// let err = {message: "failed tp fetch"};
+
+// let p1 = new Promise((resolve,reject) => {
+//     if(10>2){
+//         resolve(data);
+//     }
+//     else{
+//         reject(err);
+//     }
+
+// });
+// console.log(p1);
+
+// p1.then((result) => {
+//     console.log("i am then()" , result);
+
+// });
+// p1.catch((err) => {
+//     console.log("I am catch()",err);
+
+// });
+
+// p1.finally(() => {
+//     console.log("I am finally()");
+// });
+
+let  p1 = fetch("https://api.github.com/users");
+console.log(p1);
+
+p1.then((response) => {
+    console.log(response);
+    let p2 = response.json();
+    console.log(p2);
+    p2.then((data) => {
+        console.log(data);
+    });
+
+    p2.catch((err) => {
+        console.log(err);
+    });
+});
+
+p1.catch((err) => {
+    console.log(err);
+});
